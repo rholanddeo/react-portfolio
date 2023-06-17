63dda700547b0ac7c50ea7d4a8e8efb26444f6fd
Rholand Deo <rholanddeo210@gmail.com>
2023-06-17 22:12:08 +0700

    update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -15,10 +15,10 @@ const Banner = () => {
     <div className='container mx-auto'>
       <div>
         <h1>
-          RHOLAND <span>DEO</span>
+          {/* RHOLAND <span>DEO</span> */}
+          RHOLAND DEO EKA PUTRA
         </h1>
-      </div>
-      <div className='mb-6 text-[36px] lg:text-[60px] font-primary font-semibold uppercase loading-[1]'>
+      <div className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'>
       <span className='text-white mr-4'>Seorang</span>
       <TypeAnimation
         sequence={[
@@ -39,6 +39,24 @@ const Banner = () => {
         repeat={Infinity}
       />
       </div>
+      <p>
+      Mahasiswa Semester 7 Jurusan Teknik Informatika di Universitas Palangka Raya. IT Enthusiast, Programmer, 
+      Fullstack Web Developer. Mempelajari, merancang dan membangun aplikasi berbasis website maupun android.
+      </p>
+
+      <div className='flex max-w-max gap-x-6 items-center mb-12'>
+        <button className='btn btn-lg'>
+          Contact me
+        </button>
+        <a href='#' className='text-gradient btn-link'>
+            My Portofolio
+        </a>
+      </div>
+      <div className='flex text-[20px] gap-x-max mx-auto lg:mx-0'>
+
+      </div>
+      </div>
+
       
       <div>
         <img src={Image} alt='' className="rounded-full w-[200px] h-{100px}" />
