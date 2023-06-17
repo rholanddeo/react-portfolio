b9038557a210a34def05024e4c62b199884b9a6a
Rholand Deo <rholanddeo210@gmail.com>
2023-06-17 22:36:31 +0700

    update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -13,10 +13,13 @@ import { fadeIn } from '../variants';
 const Banner = () => {
   return <section id='home' className='section'>
     <div className='container mx-auto'>
-      <div>
-        <h1>
-          {/* RHOLAND <span>DEO</span> */}
-          RHOLAND DEO EKA PUTRA
+      <div className='flex flex-col gap-y-8 lg:flex-row'>
+
+      
+      <div className='flex-1 text-center font-secondary lg:text-left'>
+        <h1 className='text-[55px] font-bold leading-[0.8] lg:text-[110px]'>
+          RHOLAND <span>DEO</span>
+          {/* RHOLAND DEO EKA PUTRA */}
         </h1>
       <div className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'>
       <span className='text-white mr-4'>Seorang</span>
@@ -39,12 +42,13 @@ const Banner = () => {
         repeat={Infinity}
       />
       </div>
-      <p>
+      <p className='mb-8 max-w-lg mx-auto lg:mx-0'>
       Mahasiswa Semester 7 Jurusan Teknik Informatika di Universitas Palangka Raya. IT Enthusiast, Programmer, 
       Fullstack Web Developer. Mempelajari, merancang dan membangun aplikasi berbasis website maupun android.
       </p>
 
-      <div className='flex max-w-max gap-x-6 items-center mb-12'>
+      <div className='flex max-w-max gap-x-6 items-center mb-12
+      mx-auto lg:mx-0'>
         <button className='btn btn-lg'>
           Contact me
         </button>
@@ -52,8 +56,16 @@ const Banner = () => {
             My Portofolio
         </a>
       </div>
-      <div className='flex text-[20px] gap-x-max mx-auto lg:mx-0'>
-
+      <div className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'>
+          <a href='#'>
+            <FaGithub />
+          </a>
+          <a href='#'>
+            <FaInstagram />
+          </a>
+          <a href='#'>
+            <FaLinkedin />
+          </a>
       </div>
       </div>
 
@@ -61,6 +73,7 @@ const Banner = () => {
       <div>
         <img src={Image} alt='' className="rounded-full w-[200px] h-{100px}" />
       </div>
+    </div>
     </div>
     </section>;
 };
